78a8c0cdc3c19d11052fb4f5b8e71c94c0104a06
rickit2022 <thevusssteam@gmail.com>
2025-01-26 16:38:01 +0700

    added nginx installation

--- scripts/.install.sh
+++ scripts/.install.sh
@@ -30,7 +30,32 @@ full(){
 		  fc-cache -f -v
 		  echo -e "Installed fonts:\n$(fc-list | grep .local/share/fonts)"
 
+
 		  # -------------- ssh setup
+
+		  # -------------- nginx install from nginx repo
+		  sudo apt install curl gnupg2 ca-certificates lsb-release ubuntu-keyring
+		  curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
+					 | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+		  out=$(gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg)
+		  fingerprints=(
+					 "8540A6F18833A80E9C1653A42FD21310B49F6B46" 
+					 "573BFD6B3D8FBC641079A6ABABF5BD827BD9BF62" 
+					 "9E9BE90EACBCDE69FE9B204CBCDCD8A38D88A2B3"
+		  )
+		  for fingerprint in "${fingerprints[@]}"; do
+					 if [[ ! $out == *"$fingerprint"* ]];then
+								echo "Mismatched fingerprints. Please retry download."
+								break
+					 fi
+		  done
+		  echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
+					 http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
+					 | sudo tee /etc/apt/sources.list.d/nginx.list
+		  echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" \
+					 | sudo tee /etc/apt/preferences.d/99nginx
+		  sudo apt update
+		  sudo apt install nginx
    fi
 }
 
